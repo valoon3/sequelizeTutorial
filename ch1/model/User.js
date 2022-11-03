7f5c1abd6f80ac64796cb4f3b0a73c265585e6d2
valoon3 <valoon2@naver.com>
2022-11-03 22:18:01 +0900

    시퀄라이즈 튜토리얼 따라하기

---
+++ ch1/model/User.js
@@ -0,0 +1,11 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const User = sequelize.define('User', {
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: DataTypes.STRING
+    }
+})
